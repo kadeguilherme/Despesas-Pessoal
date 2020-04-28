ad36de6b3916a472cdc5d5934ef15da8465b8958
kadeguilherme <kadeguilherme@gmail.com>
2020-04-27 23:55:01 -0300

    Colocando SingleChildScrollView na classe main e na trnsacao_lista

--- lib/components/transcao_lista.dart
+++ lib/components/transcao_lista.dart
@@ -8,51 +8,56 @@ class TransacaoLista extends StatelessWidget {
   TransacaoLista(this.transacao);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transacao.map((tr) {
-        return (Row(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.symmetric(
-                horizontal: 15,
-                vertical: 10,
-              ),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.purple,
-                  width: 2,
-                ),
-              ),
-              padding: EdgeInsets.all(10),
-              child: Text(
-                'R${tr.value.toStringAsFixed(2)}',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple),
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: transacao.map((tr) {
+            return (Row(
               children: <Widget>[
-                Text(
-                  tr.title,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    ),
+                  ),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'R${tr.value.toStringAsFixed(2)}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple),
                   ),
                 ),
-                Text(
-                  DateFormat('d MMM y (E)').format(tr.date),
-                  style: TextStyle(
-                    color: Colors.grey,
-                  ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      tr.title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      DateFormat('d MMM y (E)').format(tr.date),
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
                 ),
               ],
-            ),
-          ],
-        ));
-      }).toList(),
+            ));
+          }).toList(),
+        ),
+      ),
     );
   }
 }
